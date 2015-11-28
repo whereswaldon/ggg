1ef0fb3de6ea51eae7209fd59b4e3d82524c2e58
whereswaldon <christopher.waldon.dev@gmail.com>
2015-11-27 21:46:14 -0500

    Generates fascinating texture files.

--- genetic/target.go
+++ genetic/target.go
@@ -2,11 +2,16 @@ package genetic
 
 import (
 	"fmt"
-	//"image"
+	"image"
 	"image/color"
 	"image/png"
 	"os"
 )
+
+/**
+Color defines one of Red Green Blue or Alpha
+*/
+type Color int
 
 /**
 These variables define public access to the target array generated from
@@ -16,6 +21,13 @@ var (
 	Target       [][]uint8
 	TargetWidth  int
 	TargetHeight int
+)
+
+const (
+	RED   Color = 1
+	GREEN Color = 2
+	BLUE  Color = 3
+	ALPHA Color = 4
 )
 
 /**
@@ -57,6 +69,8 @@ func SetTarget(fileName string) error {
 	fmt.Printf("Image At(0,0): %s\n", image.At(bounds.Min.X, bounds.Min.Y))
 
 	fmt.Printf("Target dimensions: width=%d height=%d\n", width, height)
+
+	populateTargetFromImage(image)
 	return nil
 }
 
@@ -67,7 +81,70 @@ func setUpTarget(targetHeight, targetWidth int) {
 	Target = make([][]uint8, targetHeight)
 	TargetHeight = targetHeight
 	TargetWidth = targetWidth
-	for i := range target {
-		target[i] = make([]uint8, targetWidth)
+	for i := range Target {
+		Target[i] = make([]uint8, targetWidth)
 	}
+}
+
+/**
+Populate the contents of the Target with data from the Image
+*/
+func populateTargetFromImage(image image.Image) {
+	var color color.Color
+	var red, green, blue, alpha uint32
+	for y := range Target {
+		for x := range Target[y] {
+			color = image.At(x, y)
+			red, green, blue, alpha = color.RGBA()
+			Target[y][x] = uint8(red)
+			fmt.Printf("Extracted rgba(%d,%d,%d,%d)\n", red, green, blue, alpha)
+		}
+	}
+}
+
+/**
+WriteMonochromePNG creates a png file with the given name out of the given data array.
+It interprets the data array as the provided Color in the RGBA model
+*/
+func WriteMonochromePNG(fileName string, data [][]uint8, color Color) error {
+	switch color {
+	case RED:
+		return writeRedPNG(fileName, data)
+	case GREEN:
+		//return writeGreenPNG(fileName, data)
+	case BLUE:
+		//return writeBluePNG(fileName, data)
+	case ALPHA:
+		//return writeAlphaPNG(fileName, data)
+	default:
+		fmt.Println("Unknown color type.")
+	}
+	return nil
+}
+
+/**
+writeRedPNG creates a png file with the contents of the data array as its RED values
+and with GREEN and BLUE at zero. It's alpha will be maximum for all values.
+*/
+func writeRedPNG(fileName string, data [][]uint8) error {
+	rect := image.Rect(0, 0, TargetWidth, TargetHeight)
+	img := image.NewRGBA(rect)
+	for y := range data {
+		for x := range data[y] {
+			img.SetRGBA(x, y, color.RGBA{data[y][x], 0, 0, 255})
+		}
+	}
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	err = png.Encode(file, img)
+	if err != nil {
+		return err
+	}
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -15,10 +15,18 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
+		err = genetic.WriteMonochromePNG("red.png", genetic.Target, genetic.RED)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 
 	//Test out the Member implementation
 	m := genetic.NewMember()
 	fmt.Printf("%s\n", m)
 	fmt.Printf("%#v\n", m.CreateDataArray(23, 23))
+	err := genetic.WriteMonochromePNG("random.png", m.CreateDataArray(genetic.TargetHeight, genetic.TargetWidth), genetic.RED)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
