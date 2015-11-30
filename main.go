d4c5aeb2e04dc533acfe1a65bc9e33ee145ab656
whereswaldon <christopher.waldon.dev@gmail.com>
2015-11-29 23:36:16 -0500

    Change division by zero recovery. Make populations sort themselves by fitness.

--- main.go
+++ main.go
@@ -21,38 +21,7 @@ func main() {
 		}
 	}
 
-	//Test out the Member implementation
-	r := genetic.NewMember()
-	g := genetic.NewMember()
-	b := genetic.NewMember()
-	for i := 0; i < 100; i++ {
-		fmt.Printf("Red: %s\n", r)
-		fmt.Printf("Blue: %s\n", b)
-		fmt.Printf("Green: %s\n", g)
-		/*	g := genetic.NewMember()
-			fmt.Printf("Green: %s\n", g)
-			b := genetic.NewMember()
-			fmt.Printf("Blue: %s\n", b)
-			a := genetic.NewMember()
-			fmt.Printf("Alpha: %s\n", a)
-			err := genetic.WriteMultichromePNG("random.png",
-				r.CreateDataArray(genetic.TargetHeight, genetic.TargetWidth),
-				g.CreateDataArray(genetic.TargetHeight, genetic.TargetWidth),
-				b.CreateDataArray(genetic.TargetHeight, genetic.TargetWidth),
-				a.CreateDataArray(genetic.TargetHeight, genetic.TargetWidth))
-		*/
-		err := genetic.WriteMultichromePNG(fmt.Sprintf("random-%d.png", i),
-			r.GetData(), g.GetData(), b.GetData(), nil)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Fitness: %d\n", r.GetFitness())
-		r.Mutate()
-		g.Mutate()
-		b.Mutate()
-		/*
-			p := genetic.NewPopulation(100)
-			fmt.Println(p)
-		*/
-	}
+	//test out new population
+	p := genetic.NewPopulation(100)
+	fmt.Println(p.Size())
 }
