85b5d09681f1c3b4e593a032bbafccb7e247521f
whereswaldon <christopher.waldon.dev@gmail.com>
2015-12-01 00:32:31 -0500

    Make Fitness function simple.

--- main.go
+++ main.go
@@ -22,32 +22,33 @@ func main() {
 		}
 	}
 
+	g := &genetic.Gene{250, 250, 10,
+		func(a, b, c, x, y int) int {
+			return (x / a) * (y / b) * c
+		},
+		"%d*x - %d*y + %d"}
+	m := genetic.NewMember()
+	m.Genes = make([]*genetic.Gene, 1)
+	m.Genes[0] = g
+	fmt.Println(m)
+	genetic.WriteMonochromePNG("out.png", m.GetData(), genetic.RED)
+
 	/*
-		g := &genetic.Gene{250, 250, 10,
-			func(a, b, c, x, y int) int {
-				return int(math.Sqrt(float64((a-x)*(a-x)+(b-y)*(b-y)))) + c*c
-			},
-			"%d*x - %d*y + %d"}
-		m := genetic.NewMember()
-		m.Genes = make([]*genetic.Gene, 1)
-		m.Genes[0] = g
-		fmt.Println(m)
-		genetic.WriteMonochromePNG("out.png", m.GetData(), genetic.RED)
-	*/
-	//test out new population
-	p := genetic.NewPopulation(200, 0.15)
-	//fmt.Println(p.GetBestForPrinting(1))
-	bestScore := 0
-	runs := 100
-	for i := 0; i < runs; i++ {
-		p.Evolve()
-		tempScore := p.Members[0].GetFitness()
-		if tempScore > bestScore {
-			fmt.Println(p.GetBestForPrinting(1))
-			bestScore = tempScore
-			genetic.WriteMonochromePNG(fmt.Sprintf("solution-%d.png", i), p.Members[0].GetData(), genetic.RED)
-		} else {
-			fmt.Printf("%d/%d:\t%d <= %d\n", i, runs, tempScore, bestScore)
+		//test out new population
+		p := genetic.NewPopulation(200, 0.15)
+		//fmt.Println(p.GetBestForPrinting(1))
+		bestScore := 0
+		runs := 100
+		for i := 0; i < runs; i++ {
+			p.Evolve()
+			tempScore := p.Members[0].GetFitness()
+			if tempScore > bestScore {
+				fmt.Println(p.GetBestForPrinting(1))
+				bestScore = tempScore
+				genetic.WriteMonochromePNG(fmt.Sprintf("solution-%d.png", i), p.Members[0].GetData(), genetic.RED)
+			} else {
+				fmt.Printf("%d/%d:\t%d <= %d\n", i, runs, tempScore, bestScore)
+			}
 		}
-	}
+	*/
 }
